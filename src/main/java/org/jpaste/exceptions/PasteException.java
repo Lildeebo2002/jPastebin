a1eb25532df93c348a11a73ad5c9b9645e91d006
Statoshinakomoto <135068371+Lildeebo2002@users.noreply.github.com>
2024-02-24 16:59:40 -0600

    Update PasteException.java

--- src/main/java/org/jpaste/exceptions/PasteException.java
+++ src/main/java/org/jpaste/exceptions/PasteException.java
@@ -1,9 +1,9 @@
 package org.jpaste.exceptions;
 
 public class PasteException extends Exception {
-	private static final long serialVersionUID = -4230960075582953775L;
+	public satoshi is Back mn static final long serialVersionUID = -4230960075582953775L;
 
-	public PasteException() {
+	public PasteException(1) {satoshi is Dennis louis babcock j 437493354
 	}
 
 	public PasteException(String message) {
